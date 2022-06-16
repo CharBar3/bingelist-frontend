c08232ec711b96a88499acf060bade8725723534
Charles Bartlett <ownageunit@gmail.com>
2022-06-16 11:38:43 -0400

    search is working. WIP: displaying data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Search from './pages/Search';
 
 function App() {
+  
   return (
     <div className="App">
       BingeList App
+      <Search />
     </div>
   );
 }
---
+++ src/pages/Search.js
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+
+const Search = () => {
+    // state to handle the URL changing based on search
+    const [URL, setURL] = useState('')
+    const [searchBar, setSearchBarState] = useState('')
+    // api key to be moved to .env file later
+    const API_key = "b9c2c073ec0a83e5114de86de84f79b2"
+
+    const [showSearch, setShowSearch] = useState('yeet')
+
+    // api search function
+    const apiSearch = async () => {
+        const response = await fetch (URL)
+        const data = await response.json()
+        console.log(data)
+        
+        const transformedData = data.results.map(({name, media_type}, index) => {
+            // console.log({element.name})
+            return ( 
+            <div>
+            
+            <p>{name}</p>
+            </div>
+            )
+        })
+
+
+        console.log(transformedData)
+        setShowSearch(transformedData)
+    }
+   
+    // function that sets the URL called based on searchbar text
+    const handleSubmit = e => {
+        e.preventDefault()
+        const searchBarInfo = e.target.searchBar.value
+        setURL(`https://api.themoviedb.org/3/search/multi?api_key=${API_key}&language=en-US&query=${searchBarInfo}&page=1&include_adult=false`)
+        apiSearch()
+    }
+
+    return (
+        <>
+        <h1>App Search</h1>
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="searchBar"/>
+                <button type='submit'>Search</button>
+            </form>
+            {showSearch}
+        </div>
+        </>
+    )
+}
+
+export default Search
