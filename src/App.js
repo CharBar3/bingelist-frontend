8c92fe8a8ad42dff1b1ce7c0aa8bc48cace2388d
Charles Bartlett <ownageunit@gmail.com>
2022-06-17 11:50:18 -0400

    firebase changes

--- src/App.js
+++ src/App.js
@@ -6,8 +6,21 @@ import { useState, useEffect } from 'react'
 import { auth } from './services/firebase'
 
 
+
 function App() {
   const [ user, setUser ] = useState(null)
+
+  // user token add to requests to express
+// const token = await user.getIdToken()
+// console.log(token)
+
+// NEEDS TO BE ADDED TO FETCH REQUESTS TO THE BACKEND (TOEKN IS ATTACHED TO USER)
+// const response = await fetch(URL, {
+//   method: 'GET',
+//   headers: {
+//     'Authorization': 'Bearer ' + token
+//   }
+// })
   
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(user => setUser(user))
