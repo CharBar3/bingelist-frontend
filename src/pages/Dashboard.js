ead184ae0ddbe7e092246b7f22aad0c3a358f5ff
Mariah (Tuesday) Hernandez <rahrah93@gmail.com>
2022-06-22 10:21:45 -0700

    cleaned code for readability

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,7 +1,7 @@
 //index page for list of movies/shows
 import { useEffect } from "react";
-import {  Link } from 'react-router-dom'
-const Dashboard = ({dashboardShows, getShows }) => {
+import { Link } from 'react-router-dom'
+const Dashboard = ({ dashboardShows, getShows }) => {
   console.log("dashboard loaded")
 
   useEffect(() => {
@@ -11,24 +11,29 @@ const Dashboard = ({dashboardShows, getShows }) => {
   // const hasData = () => {
   return (
     <>
-    <header className="dashHeader">
-      <h1>Your BingeList</h1>
-    </header>
       <div className="dashContainer">
-    {dashboardShows.map((show, i) => {
-        return (
-          <Link to={`/bingeList/${show._id}`}>
-          
-          <div key={i} className="showContainer">
-              <h1>{show.showTitle}</h1>
-              <h3>Seaons: {show.seasons.length}</h3>
-              <h3>User Rating: {show.userRating}</h3>
-          </div>
-          </Link>
-        );
-      })}
-    </div> 
-    </>  
-  )}
+        <header className="dashHeader">
+          <h1>Your BingeList</h1>
+        </header>
+        {dashboardShows.map((show, i) => {
+          return (
+            <Link to={`/bingeList/${show._id}`}>
+
+              <div key={i} className="showContainer">
+                <h1
+                  style={{
+                    backgroundImage: `url(${show.showPoster})`
+                  }}
+                >{show.showTitle}</h1>
+                <h3>Seaons: {show.seasons.length}</h3>
+                <h3>User Rating: {show.userRating}</h3>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+    </>
+  )
+}
 
 export default Dashboard
