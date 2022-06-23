434a83227418b5536bc451864b83681dfbca7b91
Charles Bartlett <ownageunit@gmail.com>
2022-06-23 19:50:50 -0400

    fixed url one last time

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ useEffect(() => {
 //   }
 // })
 
-const URL = 'https://binge-list-project.herokuapp.com/bingelist'
+const URL = 'https://binge-list-project.herokuapp.com/bingelist/'
 // const URL = 'http://localhost:4000/bingelist'
 
 
