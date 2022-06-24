d11e5480371942bd6033077f17850b0090622f44
)- <a-@-s-MacBook-Pro.local>
2022-06-24 12:00:45 -0400

    added svg icons to nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import { login, logout } from '../services/firebase'
 import { Link } from 'react-router-dom'
-// figure out how to make fabars icon appear later
-// import{FaBars, FaTimes} from 'react-icons/fa'
+import { FaAirbnb, FaBeer, FaHome, FaPlane, FaPlus, FaPowerOff } from 'react-icons/fa';
 
 function NavBar(props) {
 
@@ -15,20 +14,27 @@ function NavBar(props) {
     <div className='navBarContainer'>
       <h1 className='logo'>BingeList</h1>
       <nav>
+       
         {props.user
-          ? <p onClick={logout} className="login"> Logout</p>
-          : <p onClick={login} className="login" > Login</p>
+          ? <p onClick={logout} className="login"><FaPowerOff /> </p>
+          : <p onClick={login} className="login" > <FaPowerOff /> </p>
         }
-        <Link to="/bingelist/dashboard" className='navLink'> Home </Link>
+       
+        <Link to="/bingelist/dashboard" className='navLink'><FaHome /> </Link>
+       
         {/* <Link to="/bingelist/dashboard">Dashboard</Link> */}
-        <Link to="/bingelist/search" className='navLink'> Add New </Link>
+
+        <Link to="/bingelist/search" className='navLink'> <FaPlus /> </Link>
+        
+        
         {/* This line of code shows the user whether they are logged in or not so the ? and the : 
-      are just like if else statements */}
-        <button onClick={handleClick}  className='hamburger'>
+        are just like if else statements */}
+       
+        {/* <button onClick={handleClick}  className='hamburger'>
           <span></span>
           <span></span>
           <span></span>
-        </button>
+        </button> */}
       </nav>
     </div>
   )
